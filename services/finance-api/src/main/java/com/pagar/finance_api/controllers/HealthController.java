2546f273e2d46829e86e64fc314c074200b1a860
WelIDev <wellington.andrade.wsa@gmail.com>
2025-08-15 15:22:57 -0300

    chore: inicializa microserviço finance-api

---
+++ services/finance-api/src/main/java/com/pagar/finance_api/controllers/HealthController.java
@@ -0,0 +1,23 @@
+package com.pagar.finance_api.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+@Tag(name = "Health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "ok");
+        return ResponseEntity.ok(status);
+    }
+}
